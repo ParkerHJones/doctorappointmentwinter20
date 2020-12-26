458ec7231cc24583fc58ab3737ae27da5c7d7d49
Parker Jones <parker.harold.jones@gmail.com>
2020-12-26 10:45:17 -0700

    made doctors info and their own page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'users/index'
+  get 'users/show'
+  root "users#index"
+
+  resources :users
+
+  resources :doctors do 
+    resources :appts
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
